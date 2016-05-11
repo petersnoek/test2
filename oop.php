157cdaae3fc7325f3df3f177bb3d3a92b5397feb
Peter Snoek <petersnoek@davinci.nl>
2016-05-11 13:53:50 +0200

    added welcome message

--- oop.php
+++ oop.php
@@ -10,6 +10,7 @@ $tristan = new Student('Tristan', 'de', 'Jager', '99389420');
 $jelle = new Student('Jelle', 'van den', 'Berg', '94382394');
 $lucas = new Person('Lukas', 'kleihn', 'Hanenburgt');
 
+echo '<h1>Welkom!</h1>';
 
 // print the contents
 echo $tristan->ToString();
